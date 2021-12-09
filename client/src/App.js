c6736ff5be925e0a52aa2322a4c9d15b7f0a8599
Irina Cucereavii <irina.cucereavii@gmail.com>
2021-12-08 22:01:43 -0500

    more dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import GlobalStyles from "./GlobalStyles";
+import LandingPage from "./components/LandingPage"
+import SearchPage from "./components/SearchPage"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+
+    </>
   );
 }
 
